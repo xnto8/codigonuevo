1e375b593dc36f1189f166eb80840faa731f4ccc
xnto8 <asanchezm9@correo.uss.cl>
2024-11-08 17:27:13 -0300

    Update codigo02.py

--- codigo02.py
+++ codigo02.py
@@ -14,7 +14,8 @@ import numpy as np
 st.title('Uber pickups in NYC')
 
 DATE_COLUMN = "date/time"
-DATE_URL = ("https:// s3-us-west-2.amazonaws.com/","streamlit-demo-data/uber-raw-data-sep14.csv.gz")
+DATE_URL = ("https:// s3-us-west-2.amazonaws.com/"
+            "streamlit-demo-data/uber-raw-data-sep14.csv.gz")
 @st.cache
 def load_data(nrows):
     data = pd.read_csv(DATE_URL, nrows=nrows)
